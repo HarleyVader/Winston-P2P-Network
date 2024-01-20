61f79bd2a62597146fde07c16d804cf7f9f8efba
Melanie Elizabeth Damm <92762328+HarleyVader@users.noreply.github.com>
2024-01-20 18:38:19 +0100

    created bimbot plugin

---
+++ src/modules/plugIn/bimbot/index.js
@@ -0,0 +1,10 @@
+import { formatText, textColors } from "../../../client.js";
+export const name = "bimbot";
+export const description = "melkanea bimbot";
+export const version = "0.0001";
+export const command = (answer) => {
+  // console.log(formatText(answer, textColors.Pink));
+};
+export const exec = (clients, data) => {
+  // console.log(clients, data);
+};
